8f44eb627acf15e18d145b874810b12f8c2403c6
Adriano Rodrigues de Souza <adriano.souza@wexinc.com.br>
2018-05-08 17:22:49 -0300

    Add Features getPlanetasGalaxia e getContinentesPlaneta

--- controllers/planetarioController.js
+++ controllers/planetarioController.js
@@ -32,4 +32,75 @@ module.exports = function(app) {
             })    
         })
 
+        app.post('/planetario/galaxia/', function(req, res) {
+            console.log('retorna os galaxiass')
+    
+            res.setHeader('Access-Control-Allow-Origin', "*");
+            res.setHeader('Access-Control-Allow-Methods', 'POST');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');        
+    
+            var retorno = req.body        
+            console.log('retorno : ' + JSON.stringify(retorno))
+    
+                var planetarioDAO = new app.model.PlanetarioDAO;
+        
+                planetarioDAO.listGalaxia(function(erro, resultado){
+                    if(erro){
+                        console.log('Erro ao consultar no banco:' + erro);
+                        res.status(500).send(erro);
+                    } else {          
+                        res.location('/planetario/galaxia/');    
+                        res.status(200).json(resultado.rows);                        
+                    }
+                })    
+            })
+
+        app.post('/planetario/getPlanetasGalaxia/', function(req, res) {
+            console.log('retorna os galaxiass')
+    
+            res.setHeader('Access-Control-Allow-Origin', "*");
+            res.setHeader('Access-Control-Allow-Methods', 'POST');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');        
+    
+            var body = req.body        
+            console.log('body : ' + JSON.stringify(body))
+    
+                var planetarioDAO = new app.model.PlanetarioDAO;
+        
+                planetarioDAO.getPlanetasGalaxia(body.id, function(erro, resultado){
+                    if(erro){
+                        console.log('Erro ao consultar no banco:' + erro);
+                        res.status(500).send(erro);
+                    } else {          
+                        res.location('/planetario/galaxia/');    
+                        res.status(200).json(resultado.rows);                        
+                    }
+                })    
+        })
+
+        
+        app.post('/planetario/getContinentesPlaneta/', function(req, res) {
+            console.log('retorna os continente')
+    
+            res.setHeader('Access-Control-Allow-Origin', "*");
+            res.setHeader('Access-Control-Allow-Methods', 'POST');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');        
+    
+            var body = req.body        
+            console.log('body : ' + JSON.stringify(body))
+    
+                var planetarioDAO = new app.model.PlanetarioDAO;
+        
+                planetarioDAO.getContinentesPlaneta(body.id, function(erro, resultado){
+                    if(erro){
+                        console.log('Erro ao consultar no banco:' + erro);
+                        res.status(500).send(erro);
+                    } else {          
+                        res.location('/planetario/continente/');    
+                        res.status(200).json(resultado.rows);                        
+                    }
+                })    
+        })
+
+            
 }
--- model/PlanetarioDAO.js
+++ model/PlanetarioDAO.js
@@ -15,16 +15,37 @@ PlanetarioDAO.prototype.listPlaneta = async function(callback){
         callback(erro,resultado)
         releaseConnection(connection)
     });
-
 }
 
-PlanetarioDAO.prototype.getPlaneta = async function(id, callback){
+PlanetarioDAO.prototype.listGalaxia = async function(callback){
+    console.log('DB Connection' + createDBConnection)
     var connection = await createDBConnection()
-    connection.execute('select * from planeta where id = :id'
+    console.log('Connection' + connection)
+    connection.execute('select * from galaxia'
+    ,{}
+    ,{outFormat: oracledb.OBJECT}
+    ,(erro, resultado) => {
+        callback(erro,resultado)
+        releaseConnection(connection)
+    });
+}
+
+PlanetarioDAO.prototype.getPlanetasGalaxia = async function(id, callback){
+    var connection = await createDBConnection()
+    connection.execute('select * from planeta where ID_GALAXIA = :id'
     ,[id]
     ,{outFormat: oracledb.OBJECT}
     ,callback);
 }
+
+PlanetarioDAO.prototype.getContinentesPlaneta = async function(id, callback){
+    var connection = await createDBConnection()
+    connection.execute('select * from continente where ID_PLANETA = :id'
+    ,[id]
+    ,{outFormat: oracledb.OBJECT}
+    ,callback);
+}
+
 
 module.exports = function(){
     return PlanetarioDAO
